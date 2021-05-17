7ad3e0973fbf1abea0fc1eae63773071b5b56758
danejan <83499057+danejan@users.noreply.github.com>
2021-05-18 00:13:09 +0300

    Add files via upload
    Bu projenin üzerinde çalışmaktayım. Eksiklerini en yakın zamanda tamamlayacağım.

---
+++ JavaCampHomework4GameProject/src/Abstract/CampaignManagerService.java
@@ -0,0 +1,14 @@
+package Abstract;
+
+import Entitities.Campaign;
+
+public interface CampaignManagerService {
+
+    void CampaignAdd(Campaign campaign) ;
+
+
+    void CampaignDelete(Campaign campaign);
+
+
+    void CampaignUpdate(Campaign campaign);
+}
---
+++ JavaCampHomework4GameProject/src/Abstract/CheckUserManager.java
@@ -0,0 +1,15 @@
+package Abstract;
+
+import Entitities.User;
+public interface CheckUserManager {
+    public void CheckIfRealPerson(User user);
+}
+
+
+
+
+
+
+
+
+
